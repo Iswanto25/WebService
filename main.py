940d55d830bd2a4f8815c2fd39a104ccb185ae4a
Agung Iswanto <58076606+Iswanto25@users.noreply.github.com>
2022-03-25 21:25:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,39 @@
-from flask import Flask, request, make_response, jsonify
-from flask_restful import Resource, Api
-from flask_cors import CORS
+from flask import Flask, jsonify, request,make_response
+import os, random, string,os, random, string
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm.attributes import QueryableAttribute
-from functools import wraps
-import jwt
-import os
-import datetime
 
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "user.db"))
 app = Flask(__name__)
-api = Api(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 db = SQLAlchemy(app)
-CORS(app)
 
-filename = os.path.dirname(os.path.abspath(__file__))
-database = 'sqlite:///' + os.path.join(filename, 'user.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = database
-db = SQLAlchemy(app)
-app.config['SECRET_KEY'] = "RAHASIA"
-
-class AuthModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(25))
-    password = db.Column(db.String(15))
+class User(db.Model):
+    username = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    password = db.Column(db.String(80), unique=False, nullable=False, primary_key=False)
+    token = db.Column(db.String(225), unique=True, nullable=True, primary_key=False)
 db.create_all()
 
-class RegisterUser(Resource):
-    def post(self):
-        dataUsername = request.form.get('username')
-        dataPassword = request.form.get('password')
+@app.route('/api/v1/login', methods=['POST'])
+def auth():
+    dataUsername = request.form.get('username')
+    dataPassword = request.form.get('password')
+    acc = dataUsername and dataPassword 
+    if acc:
+        token = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+        User.query.update({'token': token})
+        db.session.commit()
+        return make_response(jsonify({"Token API":token}),200)
+    return jsonify({"msg":"Mengambil Info Token Gagal"})
 
-        if dataUsername and dataPassword:
-            dataModel = AuthModel(username=dataUsername, password=dataPassword)
-            db.session.add(dataModel)
-            db.session.commit()
-            return make_response(jsonify({"msg":"Berhasil"}), 200)
-        return jsonify({"msg":"Username/Password tidak boleh KOSONG"})
+@app.route('/api/v2/users/info', methods=['POST'])
+def users_info():
+    token = request.values.get('token')
+    acc = User.query.filter_by(token=token).first()
+    if acc:
+        return acc.username 
+    else:
+        return 'salah input token'
 
-class LoginUser(Resource):
-    def post(self):
-        dataUsername = request.form.get('username')
-        dataPassword = request.form.get('password')
-
-        queryUsername = [data.username for data in AuthModel.query.all()]
-        queryPassword = [data.password for data in AuthModel.query.all()]
-        if dataUsername in queryUsername and dataPassword in queryPassword:
-
-            token = jwt.encode(
-                {
-                    "username":queryUsername, 
-                    "exp":datetime.datetime.utcnow() + datetime.timedelta(minutes=100)
-                }, app.config['SECRET_KEY'], algorithm="HS256"
-            )
-            return make_response(jsonify({"msg":"succes", "token":token}), 200)
-        return jsonify({"msg":""})
-
-api.add_resource(RegisterUser, "/reg", methods=["POST"])
-api.add_resource(LoginUser, "/log", methods=["POST"])
-
-if __name__ == "_main_":
+if __name__ == '__main__':
     app.run(debug=True, port=4000)
